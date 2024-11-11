aef52c0ba11a8a677aceb3b3d2f31fd813752b04
LucaRossi801 <l.rossi39@studenti.unibg.it>
2024-11-11 12:31:04 +0100

    collegamento con Eclipse al DataBase

---
+++ org.eclipse.papyrus.javagen.UML/dataBase/DataBase.java
@@ -0,0 +1,33 @@
+package dataBase;
+
+import java.sql.*;
+
+public class DataBase {
+	private static void insert(Connection conn) throws SQLException {
+		String sql = "INSERT INTO Utente(Nome, Cognome, DataNascita, Eta, Username, Password) VALUES (?, ?, ?, ?, ?, ?)";
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, "Bob");
+			pstmt.setString(1, "Marley");
+			pstmt.setDate(1, Date.valueOf("2003-11-08"));
+			pstmt.setInt(1, 21);
+			pstmt.setString(1, "BobbyOne");
+			pstmt.setString(1, "BobbyGronde");
+			pstmt.executeUpdate();
+			
+			pstmt.setString(1, "Jhon");
+			pstmt.setString(1, "Abruzzi");
+			pstmt.setDate(1, Date.valueOf("1989-07-09"));
+			pstmt.setInt(1, 35);
+			pstmt.setString(1, "JhonAbru");
+			pstmt.setString(1, "TheMafiaBoss123");
+			pstmt.executeUpdate();
+		}
+	}
+	public static void main (String []args) throws SQLException {
+		String url = "MatchPointDB.db";
+		try(Connection conn = DriverManager.getConnection(url)){
+			insert(conn);
+			//query(conn);
+		}
+	}
+}
