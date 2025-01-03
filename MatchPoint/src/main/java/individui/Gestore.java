9a2f631a181bdac193911d3f9399b8e6750cef9a
LucaRossi801 <l.rossi39@studenti.unibg.it>
2025-01-03 10:11:07 +0100

    commenti alle classi di individui

--- MatchPoint/src/main/java/individui/Gestore.java
+++ MatchPoint/src/main/java/individui/Gestore.java
@@ -1,54 +1,76 @@
+/**
+ * Il package individui contiene la definizione delle classi relative agli individui.
+ */
 package individui;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.time.*;
+import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
 import dataBase.DataBase;
 
+/**
+ * La classe Gestore rappresenta un utente di tipo gestore nel sistema.
+ * Estende la classe Utente e include attributi aggiuntivi come certificazione e competenze.
+ */
 public class Gestore extends Utente {
-	
-	public String certificazione;
-	public String competenze;
-	public Gestore() {
-		super();
-	}
 
-	public static int registrazione(String nome, String cognome, String dataNascita, String email, String username, String password, String certificazioni, String competenze) {
-		String url = "jdbc:sqlite:src/main/java/dataBase/matchpointDB.db"; //connessione al database
-		int eta;
-		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	        LocalDate nascita = LocalDate.parse(dataNascita, formatter);
-	        LocalDate oggi = LocalDate.now();
-	        Period periodo = Period.between(nascita, oggi);
-	        eta= periodo.getYears();
-        String sql="SELECT Password FROM Gestore WHERE Username ='"+username+"' UNION SELECT Password FROM Giocatore WHERE Username ='"+username+"'";//creazione query
-		String ris="";
-		try  (Connection conn = DriverManager.getConnection(url)){
-			ris = DataBase.eseguiSelect(conn, sql);//esecuzione select
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} //risultato della query
-		System.out.println(ris);
-		if(ris.equals("")) {		//se username non esiste
-			System.out.println("Username valido");
-			try  (Connection conn = DriverManager.getConnection(url)){
-				DataBase.insert(conn, nome, cognome, dataNascita, eta, email, username, password, certificazioni, competenze);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} 
-			return 1;
-		}
-		
-		else {
-			System.out.println("Username non valido!");
-			return -3;
-		}
-	}
-	
-	
+    public String certificazione;
+    public String competenze;
 
+    /**
+     * Costruttore di default per la classe Gestore.
+     */
+    public Gestore() {
+        super();
+    }
+
+    /**
+     * Registra un nuovo Gestore nel sistema.
+     *
+     * @param nome          Il nome dell'utente.
+     * @param cognome       Il cognome dell'utente.
+     * @param dataNascita   La data di nascita dell'utente in formato "yyyy-MM-dd".
+     * @param email         L'email dell'utente.
+     * @param username      Lo username dell'utente.
+     * @param password      La password dell'utente.
+     * @param certificazioni Le certificazioni dell'utente.
+     * @param competenze    Le competenze dell'utente.
+     * @return 1 se la registrazione ha successo, -3 se lo username è già in uso.
+     */
+    public static int registrazione(String nome, String cognome, String dataNascita, String email, String username, String password, String certificazioni, String competenze) {
+        String url = "jdbc:sqlite:src/main/java/dataBase/matchpointDB.db"; // URL di connessione al database
+        int eta;
+
+        // Calcolo dell'età basato sulla data di nascita
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate nascita = LocalDate.parse(dataNascita, formatter);
+        LocalDate oggi = LocalDate.now();
+        Period periodo = Period.between(nascita, oggi);
+        eta = periodo.getYears();
+
+        // Query per verificare se lo username esiste già
+        String sql = "SELECT Password FROM Gestore WHERE Username ='" + username + "' UNION SELECT Password FROM Giocatore WHERE Username ='" + username + "'";
+        String ris = "";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            ris = DataBase.eseguiSelect(conn, sql); // Esecuzione della query
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Se lo username non esiste, registra il nuovo utente
+        if (ris.equals("")) {
+            try (Connection conn = DriverManager.getConnection(url)) {
+                DataBase.insert(conn, nome, cognome, dataNascita, eta, email, username, password, certificazioni, competenze);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        } else {
+            return -3;
+        }
+    }
 }
--- MatchPoint/src/main/java/individui/Giocatore.java
+++ MatchPoint/src/main/java/individui/Giocatore.java
@@ -7,55 +7,72 @@ package individui;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 import components.Campo;
 import dataBase.DataBase;
 
+/**
+ * Classe che rappresenta un giocatore nel sistema.
+ * Estende la classe Utente e aggiunge attributi e funzionalità specifiche.
+ */
 public class Giocatore extends Utente {
-	public Giocatore() {
-		super();
-	}
 
-	public Campo[] campo;
+    public Campo[] campo;
 
-	public static int registrazione(String nome, String cognome, String dataNascita, String email, String username,
-			String password, String nomeSquadra) {
-		String url = "jdbc:sqlite:src/main/java/dataBase/matchpointDB.db"; // connessione al database
-		int eta;
-		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	        LocalDate nascita = LocalDate.parse(dataNascita, formatter);
-	        LocalDate oggi = LocalDate.now();
-	        Period periodo = Period.between(nascita, oggi);
-	        eta= periodo.getYears();
-		String sql = "SELECT Password FROM Gestore WHERE Username ='" + username
-				+ "' UNION SELECT Password FROM Giocatore WHERE Username ='" + username + "'";// creazione query
-		String ris = "";
-		try (Connection conn = DriverManager.getConnection(url)) {
-			ris = DataBase.eseguiSelect(conn, sql);// esecuzione select
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} // risultato della query
-		System.out.println(ris);
-		if (ris.equals("")) { // se username non esiste
-			System.out.println("Username valido");
-			try (Connection conn = DriverManager.getConnection(url)) {
-				DataBase.insert(conn, nome, cognome, dataNascita, eta, email, username, password, nomeSquadra);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return 1;
-		}
+    /**
+     * Costruttore di default per la classe Giocatore.
+     */
+    public Giocatore() {
+        super();
+    }
 
-		else {
-			System.out.println("Username non valido!");
-			return -3;
-		}
-	}
+    /**
+     * Metodo per registrare un nuovo giocatore nel sistema.
+     *
+     * @param nome        Il nome del giocatore.
+     * @param cognome     Il cognome del giocatore.
+     * @param dataNascita La data di nascita del giocatore in formato "yyyy-MM-dd".
+     * @param email       L'email del giocatore.
+     * @param username    Lo username del giocatore.
+     * @param password    La password del giocatore.
+     * @param nomeSquadra Il nome della squadra del giocatore.
+     * @return 1 se la registrazione ha successo, -3 se lo username è già in uso.
+     */
+    public static int registrazione(String nome, String cognome, String dataNascita, String email, String username,
+                                     String password, String nomeSquadra) {
+        String url = "jdbc:sqlite:src/main/java/dataBase/matchpointDB.db"; // Connessione al database
+        int eta;
 
+        // Calcolo dell'età basato sulla data di nascita
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate nascita = LocalDate.parse(dataNascita, formatter);
+        LocalDate oggi = LocalDate.now();
+        Period periodo = Period.between(nascita, oggi);
+        eta = periodo.getYears();
+
+        // Query per verificare se lo username esiste già
+        String sql = "SELECT Password FROM Gestore WHERE Username ='" + username + "' UNION SELECT Password FROM Giocatore WHERE Username ='" + username + "'";
+        String ris = "";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            ris = DataBase.eseguiSelect(conn, sql); // Esecuzione della query
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Se lo username non esiste, registra il nuovo giocatore
+        if (ris.equals("")) {
+            try (Connection conn = DriverManager.getConnection(url)) {
+                DataBase.insert(conn, nome, cognome, dataNascita, eta, email, username, password, nomeSquadra);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        } else {
+            return -3;
+        }
+    }
 }
